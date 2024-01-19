d27f1254681d33bb5c617b2829b7fc4b67314472
Leah Veldhuisen <lveldhuisen@email.arizona.edu>
2024-01-19 16:57:52 -0700

    Update abundance_data.R

--- abundance_data.R
+++ abundance_data.R
@@ -27,5 +27,23 @@ abundance_avg <- abundance %>%
   group_by(Site, Species) %>%
   summarize(mean_abundance = mean(Total))
 
-write.csv(abundance_avg, file="phylogenetics-and-ranges/abundance_avg.csv")
+write.csv(abundance_avg, file="/Users/leahvedlhuisen/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/abundance_avg.csv")
 
+#test for differences in abundance based on range size 
+setwd("/Users/leahvedlhuisen/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/Chapter 2")
+
+results <- read.csv("results_all.csv")
+
+#remove last 2 empty rows 
+results <- results[-c(90,91),]
+
+results_certain <- results[-c(69:90),]
+
+#Kruskal wallis test 
+##only using abundance values from Veronica ---- p value is 0.8365
+kruskal.test(Mean_abundance ~ Range_size, data = results_certain)
+ggplot(results_certain, aes(x=Range_size, y=Mean_abundance)) + geom_boxplot()
+
+##including added abundance values from from my flowering data---- p value is 0.5635
+kruskal.test(Mean_abundance ~ Range_size, data = results)
+ggplot(results, aes(x=Range_size, y=Mean_abundance)) + geom_boxplot()
