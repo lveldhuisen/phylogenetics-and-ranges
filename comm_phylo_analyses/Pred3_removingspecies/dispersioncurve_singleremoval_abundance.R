67f58064a43bb9a5485cd1e4e47d5f2a784f773b
Leah Veldhuisen <77695333+lveldhuisen@users.noreply.github.com>
2024-04-16 10:23:08 -0700

    Update dispersioncurve_singleremoval_abundance.R

--- comm_phylo_analyses/Pred3_removingspecies/dispersioncurve_singleremoval_abundance.R
+++ comm_phylo_analyses/Pred3_removingspecies/dispersioncurve_singleremoval_abundance.R
@@ -237,3 +237,53 @@ fig3_all
 
 #all figures together in ggplot-------------
 ##clean up data sets to have matching column names and columns for site and metric type#####
+
+###PBM#####
+###PD####
+PD_PBM_abundance_removal = subset(PD_PBM_abundance_removal, select = -c(ntaxa,pd.obs,pd.rand.mean,pd.rand.sd,pd.obs.rank,runs) ) #remove unnecessary columns
+
+PD_PBM_abundance_removal <- PD_PBM_abundance_removal %>% 
+  rename(SES = pd.obs.z,
+    P_value = pd.obs.p) #rename columns to match other datasets 
+
+PD_PBM_abundance_removal$Type <- c("PD") #add column for metric type 
+PD_PBM_abundance_removal$Site <- c("High elevation (3380 m)") #add column for site name 
+
+###MPD####
+MPD_PBM_abundance_removal = subset(MPD_PBM_abundance_removal, select = -c(ntaxa,mpd.obs,mpd.rand.mean,mpd.rand.sd,mpd.obs.rank,runs) ) #remove unnecessary columns
+
+MPD_PBM_abundance_removal <- MPD_PBM_abundance_removal %>% 
+  rename(SES = mpd.obs.z,
+         P_value = mpd.obs.p) #rename columns to match other datasets 
+
+MPD_PBM_abundance_removal$Type <- c("MPD") #add column for metric type 
+MPD_PBM_abundance_removal$Site <- c("High elevation (3380 m)") #add column for site name 
+
+###MNTD####
+MNTD_PBM_abundance_removal = subset(MNTD_PBM_abundance_removal, select = -c(ntaxa,mntd.obs,mntd.rand.mean,mntd.rand.sd,mntd.obs.rank,runs) ) #remove unnecessary columns
+
+MNTD_PBM_abundance_removal <- MNTD_PBM_abundance_removal %>% 
+  rename(SES = mntd.obs.z,
+         P_value = mntd.obs.p) #rename columns to match other datasets 
+
+MNTD_PBM_abundance_removal$Type <- c("MNTD") #add column for metric type 
+MNTD_PBM_abundance_removal$Site <- c("High elevation (3380 m)") #add column for site name 
+
+###Pfeiler###
+PD_Pfeiler_abundance_removal = subset(PD_Pfeiler_abundance_removal, select = -c(ntaxa,pd.obs,pd.rand.mean,pd.rand.sd,pd.obs.rank,runs) ) #remove unnecessary columns
+PD_Pfeiler_abundance_removal <- PD_Pfeiler_abundance_removal %>% 
+  rename(SES = pd.obs.z,
+         P_value = pd.obs.p) #rename columns to match other datasets 
+
+
+PD_Pfeiler_abundance_removal$Type <- c("PD") #add column for metric type 
+PD_Pfeiler_abundance_removal$Site <- c("Middle elevation (3165 m)") #add column for site name
+
+###Road####
+PD_Road_abundance_removal = subset(PD_Road_abundance_removal, select = -c(ntaxa,pd.obs,pd.rand.mean,pd.rand.sd,pd.obs.rank,runs) ) #remove unnecessary columns
+PD_Road_abundance_removal <- PD_Road_abundance_removal %>% 
+  rename(SES = pd.obs.z,
+         P_value = pd.obs.p) #rename columns to match other datasets 
+
+PD_Road_abundance_removal$Type <- c("PD") #add column for metric type 
+PD_Road_abundance_removal$Site <- c("Low elevation (2815 m)") #add column for site name 
