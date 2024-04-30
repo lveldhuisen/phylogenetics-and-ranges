f63fc161a9d8709086432096c22e8c74f791472b
Leah Veldhuisen <77695333+lveldhuisen@users.noreply.github.com>
2024-04-30 10:45:26 -0700

    Update abundance_weighting.R

--- comm_phylo_analyses/Abundance_weighting/abundance_weighting.R
+++ comm_phylo_analyses/Abundance_weighting/abundance_weighting.R
@@ -13,6 +13,10 @@ SBtree <- read.tree(file = "ALLMB.tre")
 write.tree(SBtree)
 is.rooted(SBtree)
 
+#prune tree
+pruned.tree <- treedata(SBtree, unlist(community_matrix[4,community_matrix[4,]>0]), warnings = F)$phy
+plot(pruned.tree)
+
 ##make community data matrices 
 #unweighted comparison
 community_matrix <- read.table("comm_phylo_analyses/Abundance_weighting/community_matrix_unweighted.txt", 
@@ -25,8 +29,83 @@ abundance_matrix_weighted <- read.table("comm_phylo_analyses/Abundance_weighting
 #weighted by range size
 
 
-##abundance####
+##unweighted####
+###mpd####
+mpd_unweighted <- ses.mpd(community_matrix, cophenetic(pruned.tree), null.model = c("sample.pool"),
+               abundance.weighted = FALSE, runs = 5000, iterations = 5000)
+###format dataframe#####
+mpd_unweighted = subset(mpd_unweighted, select = -c(ntaxa,mpd.obs,mpd.rand.mean,mpd.rand.sd,mpd.obs.rank,runs) ) #remove unnecessary columns
+mpd_unweighted <- mpd_unweighted[-c(4),]
+mpd_unweighted <- mpd_unweighted %>% 
+  rename(SES = mpd.obs.z,
+         P_value = mpd.obs.p) #rename columns to match other datasets 
 
+mpd_unweighted$Type <- c("MPD") #add column for metric type 
+mpd_unweighted$Site <- c("Low elevation (2815 m)","Middle elevation (3165 m)",
+                         "High elevation (3380 m)") #add column for site name 
+mpd_unweighted$Weighting <- c("Unweighted") #add column for weighting
+
+###mntd###
+mntd_unweighted <- ses.mntd(community_matrix, cophenetic(pruned.tree), 
+                            null.model = c("sample.pool"),
+                 abundance.weighted=FALSE, runs = 5000, iterations = 5000)
+
+###format dataframe#####
+mntd_unweighted = subset(mntd_unweighted, 
+                        select = -c(ntaxa,mntd.obs,mntd.rand.mean,mntd.rand.sd,mntd.obs.rank,runs) ) #remove unnecessary columns
+mntd_unweighted <- mntd_unweighted[-c(4),]
+mntd_unweighted <- mntd_unweighted %>% 
+  rename(SES = mntd.obs.z,
+         P_value = mntd.obs.p) #rename columns to match other datasets 
+
+mntd_unweighted$Type <- c("MNTD") #add column for metric type 
+mntd_unweighted$Site <- c("Low elevation (2815 m)","Middle elevation (3165 m)",
+                          "High elevation (3380 m)") #add column for site name 
+mntd_unweighted$Weighting <- c("Unweighted") #add column for weighting
+
+###combine#####
+all_unweighted <- rbind(mpd_unweighted,mntd_unweighted)
+
+##abundance####
+###mpd####
+mpd_weighted_a <- ses.mpd(abundance_matrix_weighted, cophenetic(pruned.tree), null.model = c("sample.pool"),
+                          abundance.weighted = TRUE, runs = 5000, iterations = 5000)
+###format dataframe#####
+mpd_weighted_a = subset(mpd_weighted_a, select = -c(ntaxa,mpd.obs,mpd.rand.mean,mpd.rand.sd,mpd.obs.rank,runs) ) #remove unnecessary columns
+mpd_weighted_a <- mpd_weighted_a[-c(4),]
+mpd_weighted_a <- mpd_weighted_a %>% 
+  rename(SES = mpd.obs.z,
+         P_value = mpd.obs.p) #rename columns to match other datasets 
+
+mpd_weighted_a$Type <- c("MPD") #add column for metric type 
+mpd_weighted_a$Site <- c("Low elevation (2815 m)","Middle elevation (3165 m)",
+                         "High elevation (3380 m)") #add column for site name 
+mpd_weighted_a$Weighting <- c("Weighted - abundance") #add column for weighting
+
+mntd_unweighted <- ses.mntd(community_matrix, cophenetic(pruned.tree), 
+                            null.model = c("sample.pool"),
+                            abundance.weighted=FALSE, runs = 5000, iterations = 5000)
+###mntd####
+mntd_weighted_a <- ses.mntd(abundance_matrix_weighted, cophenetic(pruned.tree), 
+                            null.model = c("sample.pool"),
+                            abundance.weighted=TRUE, runs = 5000, iterations = 5000)
+
+###format dataframe#####
+mntd_weighted_a = subset(mntd_weighted_a, 
+                         select = -c(ntaxa,mntd.obs,mntd.rand.mean,mntd.rand.sd,mntd.obs.rank,runs) ) #remove unnecessary columns
+mntd_weighted_a <- mntd_weighted_a[-c(4),]
+mntd_weighted_a <- mntd_weighted_a %>% 
+  rename(SES = mntd.obs.z,
+         P_value = mntd.obs.p) #rename columns to match other datasets 
+
+mntd_weighted_a$Type <- c("MNTD") #add column for metric type 
+mntd_weighted_a$Site <- c("Low elevation (2815 m)","Middle elevation (3165 m)",
+                          "High elevation (3380 m)") #add column for site name 
+mntd_weighted_a$Weighting <- c("Weighted - abundance") #add column for weighting
+
+###combine abundance weighted dataframes####
+all_weighted_a <- rbind(mntd_weighted_a,mpd_weighted_a)
+all_df <- rbind(all_unweighted,all_weighted_a)
 
 ##range size####
 
