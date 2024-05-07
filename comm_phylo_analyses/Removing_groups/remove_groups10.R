2f68f71208067960e253e33911817b7bf93930a7
Leah Veldhuisen <77695333+lveldhuisen@users.noreply.github.com>
2024-05-07 13:48:24 -0700

    Update remove_groups10.R

--- comm_phylo_analyses/Removing_groups/remove_groups10.R
+++ comm_phylo_analyses/Removing_groups/remove_groups10.R
@@ -55,33 +55,32 @@ plot(PD_PBM_group_removal_fig)
 #Pfeiler-----------
 ###PD#####
 ##make community data matrix#### 
-PBM_groups_matrix <- read.table("comm_phylo_analyses/Removing_groups/comm_matrices/PBM_removing10_abundance.txt", 
-                                sep = "\t", header = T, row.names = 1)
+Pfeiler_groups_matrix <- read.table("comm_phylo_analyses/Removing_groups/comm_matrices/Pfeiler_group10_matrix_abundance.txt", sep = "\t", header = T, row.names = 1)
 
 ##prune tree#####
 pruned.tree <- treedata(SBtree, 
-                        unlist(PBM_groups_matrix[24,PBM_groups_matrix[24,]>0]), 
+                        unlist(Pfeiler_groups_matrix[19,Pfeiler_groups_matrix[19,]>0]), 
                         warnings = F)$phy
 write.tree(pruned.tree)
 plot(pruned.tree)
 is.rooted(pruned.tree)
 
 ###PD#####
-PD_PBM_group_removal <- ses.pd(PBM_groups_matrix, pruned.tree, 
+PD_Pfeiler_group_removal <- ses.pd(Pfeiler_groups_matrix, pruned.tree, 
                                null.model = c("sample.pool"),
-                               runs = 5000, include.root=TRUE) #all PBM PD SES is 0.89
+                               runs = 5000, include.root=TRUE) #all Pfeiler PD SES is -0.36
 
 
-PD_PBM_group_removal <- PD_PBM_group_removal[-c(23,24),]
-PD_PBM_group_removal$Group_removed <- c(1:22)
+PD_Pfeiler_group_removal <- PD_Pfeiler_group_removal[-c(18,19),]
+PD_Pfeiler_group_removal$Group_removed <- c(1:17)
 
-PD_PBM_group_removal_fig <- ggplot(data= PD_PBM_group_removal) + 
-  geom_segment( aes(x=Group_removed, xend=Group_removed, y=0.89, yend=pd.obs.z), color="grey")+
+PD_Pfeiler_group_removal_fig <- ggplot(data= PD_Pfeiler_group_removal) + 
+  geom_segment( aes(x=Group_removed, xend=Group_removed, y=-0.36, yend=pd.obs.z), color="grey")+
   geom_point(mapping = aes(x=Group_removed, y=pd.obs.z), size = 2) +
   xlab("Group of species removed") +
   ylab("SES PD") +
-  scale_y_continuous(name="SES PD", breaks = c(0, 1, 0.5, 1, 1.5,2),limits=c(0, 2))+
+  scale_y_continuous(name="SES PD", breaks = c(-1,-0.5,0, 1, 0.5, 1),limits=c(-1, 1))+
   theme_classic(14) +
-  geom_hline(yintercept = 0.89, col = "lightgrey") +
-  xlim(0,22) 
-plot(PD_PBM_group_removal_fig)
+  geom_hline(yintercept = -0.36, col = "lightgrey") +
+  xlim(0,18) 
+plot(PD_Pfeiler_group_removal_fig)
