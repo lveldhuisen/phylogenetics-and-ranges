60966415dddfbdc375a6643c492e5529eedd612f
Leah Veldhuisen <77695333+lveldhuisen@users.noreply.github.com>
2024-10-29 15:56:12 -0700

    updates

--- comm_phylo_analyses/Removing_groups/remove_groups10.R
+++ comm_phylo_analyses/Removing_groups/remove_groups10.R
@@ -20,27 +20,24 @@ is.rooted(SBtree)
 
 #PBM------------
 ##make community data matrix#### 
-PBM_groups_matrix <- read.table("comm_matrices/PBM_removing10_abundance.txt", 
-                                sep = "\t", header = T, row.names = 1)
+PBM_groups_matrix <- read.table("comm_phylo_analyses/Removing_groups/comm_matrices/PBM_removing10_abundance.txt", sep = "\t", header = T, row.names = 1)
 
 ##prune tree#####
 pruned.tree <- treedata(SBtree, 
-                        unlist(PBM_groups_matrix[24,PBM_groups_matrix[24,]>0]), 
+                        unlist(PBM_groups_matrix[29,PBM_groups_matrix[29,]>0]), 
                         warnings = F)$phy
 write.tree(pruned.tree)
 plot(pruned.tree)
 is.rooted(pruned.tree)
 
 ###PD#####
-PD_PBM_group_removal <- ses.pd(PBM_groups_matrix, pruned.tree, 
-                               null.model = c("sample.pool"),
-                                   runs = 5000, include.root=TRUE) #all PBM PD SES is 0.89
+PD_PBM_group_removal <- ses.pd(PBM_groups_matrix, pruned.tree,null.model = c("sample.pool"),runs = 5000, include.root=TRUE) #all PBM PD SES is 0.62
 
 
-PD_PBM_group_removal <- PD_PBM_group_removal[-c(23,24),]#remove all and all pbm rows
-PD_PBM_group_removal$Group_removed <- c(1:22) #add column for groups
+PD_PBM_group_removal <- PD_PBM_group_removal[-c(28,29),]#remove all and all pbm rows
+PD_PBM_group_removal$Group_removed <- c(1:27) #add column for groups
 PD_PBM_group_removal$Site <- c("High elevation (3380 m)") #add column for site 
-PD_PBM_group_removal$Baseline_PD <- c(0.89) #add column for baseline PD for grey line in fig
+PD_PBM_group_removal$Baseline_PD <- c(0.62) #add column for baseline PD for grey line in fig
 PD_PBM_group_removal = subset(PD_PBM_group_removal, 
                               select = -c(ntaxa,pd.obs,pd.rand.mean,pd.rand.sd,pd.obs.rank,runs)) #remove extra columns
 PD_PBM_group_removal <- PD_PBM_group_removal %>% 
@@ -50,25 +47,24 @@ PD_PBM_group_removal <- PD_PBM_group_removal %>%
 
 #make individual figure 
 PD_PBM_group_removal_fig <- ggplot(data= PD_PBM_group_removal) + 
-  geom_segment( aes(x=Group_removed, xend=Group_removed, y=0.89, yend=pd.obs.z), color="grey")+
-  geom_point(mapping = aes(x=Group_removed, y=pd.obs.z), size = 2) +
+  geom_segment( aes(x=Group_removed, xend=Group_removed, y=0.62, yend=SES), color="grey")+
+  geom_point(mapping = aes(x=Group_removed, y=SES), size = 2) +
   xlab("Group of species removed") +
   ylab("SES PD") +
-  scale_y_continuous(name="SES PD", breaks = c(0, 1, 0.5, 1, 1.5,2),limits=c(0, 2))+
+  scale_y_continuous(name="SES PD", breaks = c(0, 1, 0.5, 1, 1.5,2),limits=c(-0.2, 2))+
   theme_classic(14) +
-  geom_hline(yintercept = 0.89, col = "lightgrey") +
-  xlim(0,22) 
+  geom_hline(yintercept = 0.62, col = "lightgrey") +
+  xlim(0,27) 
 plot(PD_PBM_group_removal_fig)
 
 
 #Pfeiler-----------
 ###PD#####
 ##make community data matrix#### 
-Pfeiler_groups_matrix <- read.table("comm_phylo_analyses/Removing_groups/comm_matrices/Pfeiler_group10_matrix_abundance.txt", sep = "\t", header = T, row.names = 1)
+Pfeiler_groups_matrix <- read.table("comm_phylo_analyses/Removing_groups/comm_matrices/Pfeiler_group10_matrix_abundance.txt", sep = "\t", header = TRUE, row.names = 1)
 
 ##prune tree#####
-pruned.tree <- treedata(SBtree, 
-                        unlist(Pfeiler_groups_matrix[19,Pfeiler_groups_matrix[19,]>0]), 
+pruned.tree <- treedata(SBtree,unlist(Pfeiler_groups_matrix[22,Pfeiler_groups_matrix[22,]>0]), 
                         warnings = F)$phy
 write.tree(pruned.tree)
 plot(pruned.tree)
@@ -77,13 +73,13 @@ is.rooted(pruned.tree)
 ###PD#####
 PD_Pfeiler_group_removal <- ses.pd(Pfeiler_groups_matrix, pruned.tree, 
                                null.model = c("sample.pool"),
-                               runs = 5000, include.root=TRUE) #all Pfeiler PD SES is -0.36
+                               runs = 5000, include.root=TRUE) #all Pfeiler PD SES is -1.3
 
 
-PD_Pfeiler_group_removal <- PD_Pfeiler_group_removal[-c(18,19),]
-PD_Pfeiler_group_removal$Group_removed <- c(1:17)
+PD_Pfeiler_group_removal <- PD_Pfeiler_group_removal[-c(21,22),]
+PD_Pfeiler_group_removal$Group_removed <- c(1:20)
 PD_Pfeiler_group_removal$Site <- c("Middle elevation (3165 m)") #add column for site 
-PD_Pfeiler_group_removal$Baseline_PD <- c(-0.36) #add column for baseline PD for grey line in fig
+PD_Pfeiler_group_removal$Baseline_PD <- c(-1.3) #add column for baseline PD for grey line in fig
 PD_Pfeiler_group_removal = subset(PD_Pfeiler_group_removal, 
                               select = -c(ntaxa,pd.obs,pd.rand.mean,pd.rand.sd,pd.obs.rank,runs)) #remove extra columns
 PD_Pfeiler_group_removal <- PD_Pfeiler_group_removal %>% 
@@ -92,14 +88,14 @@ PD_Pfeiler_group_removal <- PD_Pfeiler_group_removal %>%
 
 #make individual figure
 PD_Pfeiler_group_removal_fig <- ggplot(data= PD_Pfeiler_group_removal) + 
-  geom_segment( aes(x=Group_removed, xend=Group_removed, y=-0.36, yend=pd.obs.z), color="grey")+
-  geom_point(mapping = aes(x=Group_removed, y=pd.obs.z), size = 2) +
+  geom_segment( aes(x=Group_removed, xend=Group_removed, y=-1.3, yend=SES), color="grey")+
+  geom_point(mapping = aes(x=Group_removed, y=SES), size = 2) +
   xlab("Group of species removed") +
   ylab("SES PD") +
-  scale_y_continuous(name="SES PD", breaks = c(-1,-0.5,0, 1, 0.5, 1),limits=c(-1, 1))+
+  scale_y_continuous(name="SES PD", breaks = c(-1.5,-1,-0.5,0, 1, 0.5, 1),limits=c(-1.7, 1))+
   theme_classic(14) +
-  geom_hline(yintercept = -0.36, col = "lightgrey") +
-  xlim(0,18) 
+  geom_hline(yintercept = -1.3, col = "lightgrey") +
+  xlim(0,20) 
 plot(PD_Pfeiler_group_removal_fig)
 
 #Road-------------
@@ -108,7 +104,7 @@ Road_groups_matrix <- read.table("comm_phylo_analyses/Removing_groups/comm_matri
 
 ##prune tree#####
 pruned.tree <- treedata(SBtree, 
-                        unlist(Road_groups_matrix[25,Road_groups_matrix[25,]>0]), 
+                        unlist(Road_groups_matrix[32,Road_groups_matrix[32,]>0]), 
                         warnings = F)$phy
 write.tree(pruned.tree)
 plot(pruned.tree)
@@ -117,12 +113,12 @@ is.rooted(pruned.tree)
 ###PD#######
 PD_Road_group_removal <- ses.pd(Road_groups_matrix, pruned.tree, 
                                    null.model = c("sample.pool"),
-                                   runs = 5000, include.root=TRUE) #all Road PD SES is -0.17
+                                   runs = 5000, include.root=TRUE) #all Road PD SES is -0.03
 
 
-PD_Road_group_removal <- PD_Road_group_removal[-c(24,25),]
-PD_Road_group_removal$Group_removed <- c(1:23)
-PD_Road_group_removal$Baseline_PD <- c(-0.17) #add column for baseline PD for grey line in fig
+PD_Road_group_removal <- PD_Road_group_removal[-c(31,32),]
+PD_Road_group_removal$Group_removed <- c(1:30)
+PD_Road_group_removal$Baseline_PD <- c(-0.03) #add column for baseline PD for grey line in fig
 PD_Road_group_removal$Site <- c("Low elevation (2815 m)") #add column for site 
 PD_Road_group_removal = subset(PD_Road_group_removal, 
                                   select = -c(ntaxa,pd.obs,pd.rand.mean,pd.rand.sd,pd.obs.rank,runs)) #remove extra columns
@@ -132,14 +128,14 @@ PD_Road_group_removal <- PD_Road_group_removal %>%
 
 #make individual figure
 PD_Road_group_removal_fig <- ggplot(data= PD_Road_group_removal) + 
-  geom_segment( aes(x=Group_removed, xend=Group_removed, y=-0.17, yend=SES), color="grey")+
+  geom_segment( aes(x=Group_removed, xend=Group_removed, y=-0.03, yend=SES), color="grey")+
   geom_point(mapping = aes(x=Group_removed, y=SES), size = 2) +
   xlab("Group of species removed") +
   ylab("SES PD") +
   scale_y_continuous(name="SES PD", breaks = c(-2,-1.5,-1,-0.5,0, 1, 0.5, 1,1.5),limits=c(-2, 1.5))+
   theme_classic(14) +
-  geom_hline(yintercept = -0.17, col = "lightgrey") +
-  xlim(0,23) 
+  geom_hline(yintercept = -0.03, col = "lightgrey") +
+  xlim(0,30) 
 plot(PD_Road_group_removal_fig)
 
 #combine into one dataset---------
@@ -155,7 +151,7 @@ allsites_PD_groups <- ggplot(data= PD_groups_allsites) +
   ylab("Standard effect size") +
   scale_y_continuous(name="Standard effect size", breaks = c(-2,-1.5, -1, -0.5, 0, 1, 0.5, 1, 1.5,2),limits=c(-2, 2))+
   theme_bw(base_size = 20) +
-  xlim(0,23) +
+  xlim(0,30) +
   geom_abline(data = PD_groups_allsites, aes(intercept = Baseline_PD, slope = 0)) +
   facet_grid(.~Site)
 plot(allsites_PD_groups)
